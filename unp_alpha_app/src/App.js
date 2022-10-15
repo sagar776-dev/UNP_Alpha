e095dbcda824ac808ee91141bea0f3e16ba48a26
anishsubedi5566 <anishsubedi5566@gmail.com>
2022-10-14 23:33:20 -0400

    added layout to all page

--- unp_alpha_app/src/App.js
+++ unp_alpha_app/src/App.js
@@ -1,12 +1,76 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 // import './components/search'
 import Search from './components/search';
 
+import 'antd/dist/antd.css';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+
 function App() {
   return (
     <div className="App">
-      <Search/>
+      {/* <Search/> */}
+      <Layout>
+    <Sider
+      breakpoint="lg"
+      collapsedWidth="0"
+      onBreakpoint={(broken) => {
+        console.log(broken);
+      }}
+      onCollapse={(collapsed, type) => {
+        console.log(collapsed, type);
+      }}
+    >
+      <div className="logo" />
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={['4']}
+        items={[UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
+          (icon, index) => ({
+            key: String(index + 1),
+            icon: React.createElement(icon),
+            label: `nav ${index + 1}`,
+          }),
+        )}
+      />
+    </Sider>
+    <Layout>
+      <Header
+        className="site-layout-sub-header-background"
+        style={{
+          padding: 0,
+        }}
+      />
+      <Content
+        style={{
+          margin: '24px 16px 0',
+        }}
+      >
+        <div
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            minHeight: 360,
+          }}
+        >
+          {/* content */}
+          <Search/>
+        
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
+  </Layout>
     </div>
   );
 }
