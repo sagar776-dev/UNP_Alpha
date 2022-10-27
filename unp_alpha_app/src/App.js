f4795393e6b97267ab0b04590e0918a46945cf66
ashay <ashay@megdap.com>
2022-10-26 22:53:01 -0400

    Fixing development issues

--- unp_alpha_app/src/App.js
+++ unp_alpha_app/src/App.js
@@ -1,13 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import './components/search'
-import Search from 'antd/lib/input/Search';
+import Searchpage from './searchpage';
+import 'antd/dist/antd.css';
+import Login from './Login';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <Search/>
-    </div>
+    <BrowserRouter>
+      <div className="App">        
+        <Routes>
+          <Route path="/">
+            <Login />
+          </Route>
+          <Route path="/home">
+            <Searchpage />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
