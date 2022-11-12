0582cf8367f340a4a64e1e1b76d1460154236ad8
anishsubedi5566 <anishsubedi5566@gmail.com>
2022-11-12 18:29:44 -0500

    added layout to kid and parent page

--- unp_alpha_app/src/pages/KidProfile.js
+++ unp_alpha_app/src/pages/KidProfile.js
@@ -1,7 +1,21 @@
-import React from 'react';
+// import React from 'react';
+// import 'antd/dist/antd.css';
+// // import './index.css';
+// import { Button, Form, Input, InputNumber } from 'antd';
+
+
+// import React from 'react';
 import 'antd/dist/antd.css';
 // import './index.css';
 import { Button, Form, Input, InputNumber } from 'antd';
+import React, { useState, useEffect } from 'react';
+import Search from '../components/search';
+import Messenger from "../components/messenger/Messenger";
+import 'antd/dist/antd.min.css';
+import { Layout, Menu } from 'antd';
+import { Link } from 'react-router-dom';
+const { Header, Content, Footer, Sider } = Layout;
+
 
 
 const layout = {
@@ -28,6 +42,82 @@ const Profilepage: React.FC = () => {
   };
 
   return (
+   
+
+
+<div >
+{/* <Search/> */}
+<Layout>      
+<Header
+  className="site-layout-sub-header-background"
+  style={{
+    padding: 0,
+    height: '10vh'
+  }}
+/>      
+<Layout>
+
+<Sider
+breakpoint="lg"
+collapsedWidth="0"
+// style={{marginTop:50}}      
+onBreakpoint={(broken) => {
+  console.log(broken);
+}}
+onCollapse={(collapsed, type) => {
+  console.log(collapsed, type);
+}}
+
+>
+{/* <div className="logo" /> */}
+<Menu
+  theme="dark"
+  mode="inline"                
+  defaultSelectedKeys={['2']}      
+
+>
+  <Menu.Item key='search'>        
+    <Link to='/home'>Search </Link>                                     
+  </Menu.Item>        
+ <Menu.Item >                 
+    <Link to='/profile'>Parent Profile   </Link>   
+  </Menu.Item>
+  <Menu.Item >                 
+    <Link to='/Kidprofile'>Kid Profile </Link>   
+  </Menu.Item>
+  <Menu.Item >                 
+    <Link to='/inbox'>Inbox </Link>    
+  </Menu.Item>
+  
+  <Menu.Item key='messenger'>   
+    <Link to='/messenger'>Messenger </Link>                                                             
+  </Menu.Item>
+  <Link to='/login'>
+  <Menu.Item style={{
+  position: 'absolute',
+  bottom: 0,
+  zIndex: 1,
+  transition: 'all 0.2s',
+}}
+>                    
+    Sign Out
+  </Menu.Item>
+  </Link>
+</Menu>
+</Sider>
+<Content
+  style={{
+     margin: '24px 16px 0',
+  }}
+>
+  <div
+    className="site-layout-background"
+    style={{
+      padding: 24,
+      minHeight: '90vh',
+    }}
+  >
+    {/* content */}
     <div>
       Kid's Profile
     <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
@@ -68,6 +158,12 @@ const Profilepage: React.FC = () => {
       </Form.Item>
     </Form>
     </div>
+  
+  </div>
+</Content>
+</Layout>
+</Layout>
+</div>
   );
 };
 
