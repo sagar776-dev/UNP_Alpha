3957c9f6c24a30ab4e22f93b4d593e74aa549b16
fcooper98 <farokh@farokhs-air.mynetworksettings.com>
2022-11-13 01:50:00 -0500

    ics file generation

--- Backend/routes/index.route.js
+++ Backend/routes/index.route.js
@@ -3,7 +3,7 @@ const express = require('express');
 const registrationRoutes = require('./registration');
 const parents = require('./parents');
 const kid = require('./kid');
-
+const post = require('./post');
 // router.get('/', (req, res) => res.send('It works.'));
 // router.get('/registration/', (req, res) => res.send());
 
@@ -12,6 +12,7 @@ const constructorMethod = (app) => {
     app.use('/register', registrationRoutes);
     app.use('/parents', parents);
     app.use('/kid', kid);
+    app.use('/post', post);
 
     app.use('*', (req, res) => {
       res.status(404).json({error: 'Not found'});
--- Backend/routes/post.js
+++ Backend/routes/post.js
@@ -2,6 +2,7 @@ const { Route } = require("express");
 const express = require("express");
 const router = express.Router();
 const data = require("../data/index");
+const { getCurrentDate } = require("../util/helper");
 
 const registrationData = data.registration;
 
@@ -33,7 +34,36 @@ router
     res.status(404).json(e);
   });
 
+  router.route("/generateics").get(async (req, res) => {
+    const fs = require("fs");
+    var currentdate = new Date();
+    var date1= currentdate.toISOString().replace('-','').replace('-','').replace('.','').replace('.','').replace(':','').replace(':','').replace('Z','').substring(0,15); 
+  //  var date1= +currentdate.getFullYear().toLocaleString()+ (currentdate.getMonth()+1).toLocaleString();
+ const data='BEGIN:VCALENDAR\n'+
+ 'VERSION:2.0\n'+
+ 'BEGIN:VTIMEZONE\n'+
+ 'TZID:Europe/Berlin\n'+
+ 'X-LIC-LOCATION:Europe/Berlin\n'+
+ 'BEGIN:DAYLIGHT\n'+
+ 'TZNAME:CEST\n'+
+ 'DTSTART:'+date1+'\n'+  //19700329T020000
+ 'BEGIN:STANDARD\n'+
+ 'TZOFFSETFROM:+0200\n'+
+ 'TZOFFSETTO:+0100\n'+
+ 'TZNAME:CET\n'+
+ 'DTSTART:19701025T030000\n'+
+ 'RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=10\n'+
+ 'END:STANDARD\n'+
+ 'END:VTIMEZONE\n'+
+ 'BEGIN:VEVENT\n';
+// //'DTSTAMP:20140107T121503Z';\n'
+fs.writeFile(__dirname +"/sample.ics", data, { flag: 'w' } ,(err) => {
+   if (err) {console.log(err.message); throw err;}
+   console.log("Completed!");
+});
 
+    res.sendFile(__dirname + "/sample.ics");
+  });
   
 
 router.route("/getAllUsers").get(async (req, res) => {
