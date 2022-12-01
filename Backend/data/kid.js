164515a82089ba285fbec6711aa2412d6d0cef71
anishsubedi5566 <anishsubedi5566@gmail.com>
2022-11-30 22:13:11 -0500

    added test case

--- Backend/data/kid.js
+++ Backend/data/kid.js
@@ -3,17 +3,29 @@ const kid = require('../model/kid');
 
 
 
+// const serachKidByFilters = async (postData) => {
+//   try {
+//     console.log("Reached here inside data", postData)
+//     const parentInfolocation = await kid.findAll({ where: { location: postData.location } });
+//     console.log("This is parentInfoLocation", parentInfolocation)
+//     // const parentInfograde = await kid.findOne({ where: { grade: postData.grade } });
+//     // const parentInfogender = await kid.findOne({ where: { location: postData.location } });
+//     // const parentInfoschool = await kid.findOne({ where: { location: postData.location } });
+//     // let finalresult = parentInfolocation + parentInfograde + parentInfogender + parentInfoschool
+//     if (!parentInfolocation) throw 'Could not find parent of that location!'
+//     // console.log("finalresult", [parentInfolocation.dataValues + parentInfograde.dataValues])
+//     return (parentInfolocation)
+//   } catch (error) {
+//     return error
+//   }
+// };
+
 const serachKidByFilters = async (postData) => {
   try {
     console.log("Reached here inside data", postData)
-    const parentInfolocation = await kid.findAll({ where: { location: postData.location } });
-    console.log("This is parentInfoLocation", parentInfolocation)
-    // const parentInfograde = await kid.findOne({ where: { grade: postData.grade } });
-    // const parentInfogender = await kid.findOne({ where: { location: postData.location } });
-    // const parentInfoschool = await kid.findOne({ where: { location: postData.location } });
-    // let finalresult = parentInfolocation + parentInfograde + parentInfogender + parentInfoschool
+    const parentInfolocation = await kid.findOne({ where: { location: postData.location } });
     if (!parentInfolocation) throw 'Could not find parent of that location!'
-    // console.log("finalresult", [parentInfolocation.dataValues + parentInfograde.dataValues])
+    console.log("finalresult",parentInfolocation )
     return (parentInfolocation)
   } catch (error) {
     return error
@@ -23,5 +35,6 @@ const serachKidByFilters = async (postData) => {
 
 module.exports = {
     serachKidByFilters,
+    // serachKidByLocation
     
 };
